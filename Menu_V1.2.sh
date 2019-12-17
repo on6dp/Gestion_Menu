9b8d3c86e75dc4217ec7962e1550e754b9f5c860
Bruno <30460704+F1PTL@users.noreply.github.com>
2019-12-17 12:24:48 +0100

    Add files via upload

---
+++ Menu_V1.2.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+#===================================
+# Creation le 09/12/2019
+# Par F1PTL Bruno
+# Version 1.2
+#===================================
+#####################################
+# Variables #
+#############
+REP_COURANT=`pwd`
+FIC_REF="Fichiers_Ini_Ref"
+FIC_REF_TAR="Fichiers_Ini_Ref_V1.tar"
+#####################################
+if [ $(id -u) -ne 0 ]
+        then
+        echo ""
+        echo "==> Lancement avec les droits root"
+        exit 1
+fi
+
+while : ; do
+OPTION=$(whiptail --title "F1PTL Gestion_Menu Version 1.2" --menu " Votre choix ?" 20 50 8 \
+1 "Installation DVSwitch Server" \
+2 "Modification DVSwitch Server" \
+3 "Service DVSwitch Server" \
+4 "Log DVSwitch Server" \
+5 "Installation HBlink3 et HBmonitor" \
+6 "Modification HBlink3 et HBmonitor" \
+7 "Service HBlink3 et HBmonitor" \
+8 "Redemarrage RaspBerryPi" 3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+
+if [ $exitstatus != 0 ]; then
+    echo "==> vous avez annule"; break;
+fi
+
+case $OPTION in
+1) ${REP_COURANT}/Installation_DVSwitch_V3.10.sh ;;
+2) ${REP_COURANT}/Modification_DVSwitch_V1.0.sh ;;
+3) ${REP_COURANT}/Service_DVSwitch_V1.1.sh ;;
+4) ${REP_COURANT}/Log_DVSwitch_V1.0.sh ;;
+5) ${REP_COURANT}/Installation_HB_V1.0.sh ;;
+6) ${REP_COURANT}/Modification_HB_V1.0.sh ;;
+7) ${REP_COURANT}/Service_HBlink3_V1.0.sh ;;
+8) if (whiptail --title " Redemarrage RaspBerryPI  Oui / Non" --yesno "Redemarrage du RaspBerryPI ?" 10 60) then
+	echo "Demarrage en cours ..." ; reboot
+	else
+	echo "==> vous avez annule"; exit 1
+   fi ;;
+
+esac
+done
+exit 0
---
+++ Service_HBlink3_V1.0.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+#===================================
+# Creation le 03/12/2019
+# Par F1PTL Bruno
+# Version 1.0
+#===================================
+#####################################
+# Variables #
+#############
+REP_COURANT=`pwd`
+FIC_REF="Fichiers_Ini_Ref"
+FIC_REF_TAR="Fichiers_Ini_Ref_V1.tar"
+#####################################
+if [ $(id -u) -ne 0 ]
+        then
+        echo ""
+        echo "==> Lancement avec les droits root"
+        exit 1
+fi
+
+while : ; do
+OPTION=$(whiptail --title "F1PTL Service HBlink3 Version 1.0" --menu "		Choisir le Service" 25 60 7 \
+1 "Arret Service HBlink3" \
+2 "Demarrage Service HBlink3" \
+3 "Status Service HBlink3 (5sec)" \
+4 "Arret Service HBmonitor" \
+5 "Demarrage Service HBmonitor" \
+6 "Status Service HBmonitor (5sec)" \
+7 "Reload Services" 3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+
+if [ $exitstatus != 0 ]; then
+    echo "==> vous avez annule"; break;
+fi
+
+case $OPTION in
+1)
+sudo systemctl stop hblink3.service ;;
+2)
+sudo systemctl start hblink3.service ;;
+3)
+clear;sudo systemctl status hblink3.service;sleep 5 ;;
+4)
+sudo systemctl stop hbmonitor.service ;;
+5)
+sudo systemctl start hbmonitor.service ;;
+6)
+clear;sudo systemctl status hbmonitor.service;sleep 5 ;;
+7)
+sudo systemctl daemon-reload ;;
+
+
+esac
+done
+exit 0
