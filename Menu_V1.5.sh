a4fea93e4f170252c9c8f4b90f4474afbb8c5487
Bruno <30460704+F1PTL@users.noreply.github.com>
2019-12-30 14:05:55 +0100

    Add files via upload

--- Menu_V1.5.sh
+++ Menu_V1.5.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #
 #===================================
-# Creation le 29/12/2019
+# Creation le 26/12/2019
 # Par F1PTL Bruno
-# Version 1.5
 #===================================
 #####################################
 # Variables #
 #############
+Version="1.5"
 REP_COURANT=`pwd`
 FIC_REF="Fichiers_Ini_Ref"
-FIC_REF_TAR="Fichiers_Ini_Ref_V1.tar"
 #####################################
 if [ $(id -u) -ne 0 ]
         then
@@ -20,15 +19,17 @@ if [ $(id -u) -ne 0 ]
 fi
 
 while : ; do
-OPTION=$(whiptail --title "F1PTL Gestion_Menu Version 1.5" --menu " Votre choix ?" 20 50 8 \
+OPTION=$(whiptail --title "F1PTL Gestion_Menu Version ${Version}" --menu " Votre choix ?" 20 50 10 \
 1 "Installation DVSwitch Server" \
 2 "Modification DVSwitch Server" \
 3 "Service DVSwitch Server" \
 4 "Log DVSwitch Server" \
-5 "Installation HBlink3 et HBmonitor" \
-6 "Modification HBlink3 et HBmonitor" \
-7 "Service HBlink3 et HBmonitor" \
-8 "Redemarrage RaspBerryPi" 3>&1 1>&2 2>&3)
+5 "Installation Users DVSwitch Server" \
+6 "Suppression Users DVSwitch Server" \
+7 "Installation HBlink3 et HBmonitor" \
+8 "Modification HBlink3 et HBmonitor" \
+9 "Service HBlink3 et HBmonitor" \
+10 "Redemarrage RaspBerryPi" 3>&1 1>&2 2>&3)
  
 exitstatus=$?
 
@@ -41,10 +42,12 @@ case $OPTION in
 2) ${REP_COURANT}/Modification_DVSwitch_V1.0.sh ;;
 3) ${REP_COURANT}/Service_DVSwitch_V1.1.sh ;;
 4) ${REP_COURANT}/Log_DVSwitch_V1.0.sh ;;
-5) ${REP_COURANT}/Installation_HB_V1.0.sh ;;
-6) ${REP_COURANT}/Modification_HB_V1.0.sh ;;
-7) ${REP_COURANT}/Service_HBlink3_V1.0.sh ;;
-8) if (whiptail --title " Redemarrage RaspBerryPI  Oui / Non" --yesno "Redemarrage du RaspBerryPI ?" 10 60) then
+5) ${REP_COURANT}/Installation_Users_DVSwitch.sh ;;
+6) ${REP_COURANT}/Suppression_Users_DVSwitch.sh ;;
+7) ${REP_COURANT}/Installation_HB_V1.0.sh ;;
+8) ${REP_COURANT}/Modification_HB_V1.0.sh ;;
+9) ${REP_COURANT}/Service_HBlink3_V1.0.sh ;;
+10) if (whiptail --title " Redemarrage RaspBerryPI  Oui / Non" --yesno "Redemarrage du RaspBerryPI ?" 10 60) then
 	echo "Demarrage en cours ..." ; reboot
 	else
 	echo "==> vous avez annule"; exit 1
