740269c863f785f7a207d66026168720e28b9735
Bruno <30460704+F1PTL@users.noreply.github.com>
2020-02-21 12:13:29 +0100

    Add files via upload

---
+++ Upgrade_DVSwitch.sh
@@ -0,0 +1,138 @@
+#!/bin/sh
+#
+#===================================
+# Creation le 21/02/2020
+# Par F1PTL Bruno
+# Version 1.0
+#===================================
+#####################################
+# Variables #
+#############
+REP_COURANT=`pwd`
+#####################################
+if [ $(id -u) -ne 0 ]
+        then
+        echo ""
+        echo "==> Lancement avec les droits root"
+        exit 1
+fi
+
+whiptail --title "F1PTL UPGRADE Version 1.0" --msgbox "Upgrade des logiciels Analog_Bridge et MMDVM_Bridge.\n\n Appuyer sur la touche Entree pour continuer." 10 60
+
+clear
+echo "==================================================================="
+echo "==> Lancement de l upgrade des logiciels Analog et MMDVM_Bridge <=="
+echo "==================================================================="
+sleep 3
+
+echo ""
+echo "######################################"
+echo "==> Creation des repertoires de depots"
+echo "######################################"
+mkdir -p /depot/DVSwitch
+echo "mkdir -p /depot/DVSwitch"
+
+echo ""
+echo "#######################"
+echo "==> Depot des logiciels"
+echo "#######################"
+cd /depot/DVSwitch
+git clone https://github.com/DVSwitch/MMDVM_Bridge.git
+git clone https://github.com/DVSwitch/Analog_Bridge.git
+
+echo ""
+echo "###########################################"
+echo "==> Deplacement des logiciels dans /usr/src"
+echo "###########################################"
+cd /depot/DVSwitch
+cp -rf MMDVM_Bridge /usr/src
+echo "cp -rf MMDVM_Bridge /usr/src"
+rm -rf /depot/DVSwitch/MMDVM_Bridge
+
+cd /depot/DVSwitch
+cp -rf Analog_Bridge /usr/src
+echo "cp -rf Analog_Bridge /usr/src"
+rm -rf /depot/DVSwitch/Analog_Bridge
+
+echo ""
+echo "#############################################"
+echo "==> Arret des services Analog et MMDVM_Bridge"
+echo "#############################################"
+sudo systemctl stop analog_bridge.service
+echo "sudo systemctl stop analog_bridge.service"
+sleep 1
+sudo systemctl stop mmdvm_bridge.service
+echo "sudo systemctl stop mmdvm_bridge.service"
+sleep 1
+
+echo ""
+echo "##################################################"
+echo "==> Sauvegarde des fichiers Analog et MMDVM_Bridge"
+echo "##################################################"
+mkdir -p /usr/src/svg
+cp -rf /opt/Analog_Bridge /usr/src/svg/Analog_Bridge
+echo "cp -rf /opt/Analog_Bridge /usr/src/svg/Analog_Bridge"
+cp -rf /opt/MMDVM_Bridge /usr/src/svg/MMDVM_Bridge
+echo "cp -rf /opt/MMDVM_Bridge /usr/src/svg/MMDVM_Bridge"
+
+echo ""
+echo "#############################################"
+echo "==> Copie des binaires MMDVM et Analog_Bridge"
+echo "#############################################"
+cd /usr/src/MMDVM_Bridge/bin
+PROC=`uname -m`
+case ${PROC} in
+	armv7l) cp -rf MMDVM_Bridge.armhf ../MMDVM_Bridge
+		chmod 755 /usr/src/MMDVM_Bridge/MMDVM_Bridge ;;
+			
+	x86_64) cp -rf MMDVM_Bridge.amd64 ../MMDVM_Bridge
+		chmod 755 /usr/src/MMDVM_Bridge/MMDVM_Bridge ;;
+			
+	     *) echo "Modele de serveur non pris en charge"
+		exit 1 ;;
+esac
+cp -rf /usr/src/MMDVM_Bridge/MMDVM_Bridge /opt/MMDVM_Bridge
+echo "cp -rf /usr/src/MMDVM_Bridge/MMDVM_Bridge /opt/MMDVM_Bridge"
+
+
+cd /usr/src/Analog_Bridge/bin
+PROC=`uname -m`
+case ${PROC} in
+	armv7l) cp -rf Analog_Bridge.armhf ../Analog_Bridge
+		chmod 755 /usr/src/Analog_Bridge/Analog_Bridge ;;
+			
+	x86_64) cp -rf Analog_Bridge.amd64 ../Analog_Bridge
+		chmod 755 /usr/src/Analog_Bridge/Analog_Bridge ;;
+			
+	     *) echo "Modele de serveur non pris en charge"
+		exit 1 ;;
+esac
+cp -rf /usr/src/Analog_Bridge/Analog_Bridge /opt/Analog_Bridge
+echo "cp -rf /usr/src/Analog_Bridge/Analog_Bridge /opt/Analog_Bridge"
+cp -rf /usr/src/Analog_Bridge/dvswitch.sh /opt/Analog_Bridge
+echo "cp -rf /usr/src/Analog_Bridge/dvswitch.sh /opt/Analog_Bridge"
+
+
+echo ""
+echo "#############################################"
+echo "==> Demarrage des services Analog et MMDVM_Bridge"
+echo "#############################################"
+sudo systemctl start analog_bridge.service
+echo "sudo systemctl start analog_bridge.service"
+sleep 1
+sudo systemctl start mmdvm_bridge.service
+echo "sudo systemctl start mmdvm_bridge.service"
+sleep 1
+
+echo ""
+echo ""
+echo "==========================================================="
+echo " Fin d upgrade des logiciels Analog_Bridge et MMDVM_Bridge"
+echo "==========================================================="
+
+
+echo ""
+echo "##########################"
+echo "==> Fin d upgrade <=="
+echo "##########################"
+
