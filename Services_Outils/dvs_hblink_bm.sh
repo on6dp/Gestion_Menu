5ddec15dbca27d270af48248a984de9eccfb40e9
Bruno <30460704+F1PTL@users.noreply.github.com>
2020-05-05 19:10:09 +0200

    Update dvs_hblink_bm.sh

--- Services_Outils/dvs_hblink_bm.sh
+++ Services_Outils/dvs_hblink_bm.sh
@@ -5,4 +5,4 @@
 sudo systemctl stop mmdvm_bridge_hblink.service
 sudo systemctl start mmdvm_bridge.service
 sleep 2
-/opt/Analog_Bridge/dvswitch.sh message "Transfert HBlink vers BM"
+/opt/MMDVM_Bridge/dvswitch.sh message "Transfert HBlink vers BM"
